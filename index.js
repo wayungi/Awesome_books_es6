35e423a2fb233f21f9e8c7ee747390d782e86430
wayungi <fwayungi@gmail.com>
2022-07-04 23:59:40 +0300

    Remove ui.js imports

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { addBook, deleteBook, getBooks } from './modules/books.js';
 import Book from './modules/book.js';
-import { displayAddNew } from './modules/ui.js';
 
 const form = document.getElementById('add_book_form');
 
@@ -11,37 +10,6 @@ const contact = document.getElementById('contact_id');
 const addNewBookPage = document.getElementById('add_new_book');
 const contactPage = document.getElementById('contact_us');
 const listBooksPage = document.getElementById('list_books');
-
-addNew.addEventListener('click', () => displayAddNew({
-  addNew,
-  list,
-  contact,
-  addNewBookPage,
-  contactPage,
-  listBooksPage,
-}));
-
-
-
-/* {
-    ClickedLink,
-    linkHolder1,
-    linkHolder2,
-    activePage,
-    pageHolder1,
-    pageHolder2,
-  }*/ 
-  
-//   listBooksPage.classList.add('hide');
-//   contactPage.classList.add('hide');
-//   addNewBookPage.classList.remove('hide');
-
-//   list.classList.remove('active');
-//   contact.classList.remove('active');
-//   addNew.classList.add('active');
-
-//  return null;
-//});
 
 form.addEventListener('submit', () => {
   const title = document.getElementById('title').value;
--- modules/ui.js
+++ modules/ui.js
@@ -8,23 +8,41 @@ const displayAddNew = (valuesObj) => {
     listBooksPage,
   } = valuesObj;
 
-  addNew.addEventListener('click', () => {
-    // pages
-    listBooksPage.classList.add('hide');
-    contactPage.classList.add('hide');
-    addNewBookPage.classList.remove('hide');
+  // pages
+  listBooksPage.classList.add('hide');
+  contactPage.classList.add('hide');
+  addNewBookPage.classList.remove('hide');
 
-    // links
-    contact.classList.remove('active');
-    list.classList.remove('active');
-    addNew.classList.add('active');
-    return null;
-  });
+  // links
+  contact.classList.remove('active');
+  list.classList.remove('active');
+  addNew.classList.add('active');
+  return null;
 };
 
-const dummy = () => null;
+const displayList = (valuesObj) => {
+  const {
+    list,
+    addNew,
+    contact,
+    listBooksPage,
+    contactPage,
+    addNewBookPage,
+  } = valuesObj;
+
+  // pages
+  addNewBookPage.classList.add('hide');
+  contactPage.classList.add('hide');
+  listBooksPage.classList.remove('hide');
+
+  // links
+  list.classList.remove('active');
+  contact.classList.remove('active');
+  addNew.classList.add('active');
+  return null;
+};
 
 export {
-  toggleViews,
-  dummy,
+  displayAddNew,
+  displayList,
 };
