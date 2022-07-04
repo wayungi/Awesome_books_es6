f7e07ac6c8c8ae67360e7f57474c36dea0248d56
wayungi <fwayungi@gmail.com>
2022-07-04 23:14:11 +0300

    Add EventListener to addNew link

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ import Book from './modules/book.js';
 
 const form = document.getElementById('add_book_form');
 
+const list = document.getElementById('list_id');
+const addNew = document.getElementById('add_new_id');
+const contact = document.getElementById('contact_id');
+
+const addNewBookPage = document.getElementById('add_new_book');
+const contactPage = document.getElementById('contact_us');
+const listBooksPage = document.getElementById('list_books');
+
+addNew.addEventListener('click', () => {
+  listBooksPage.classList.add('hide');
+  contactPage.classList.add('hide');
+  addNewBookPage.classList.remove('hide');
+
+  list.classList.remove('active');
+  contact.classList.remove('active');
+  addNew.classList.add('active');
+
+  return null;
+});
+
 form.addEventListener('submit', () => {
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
