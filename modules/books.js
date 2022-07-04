b9caeb4703a5ad15135e7d942da405ce7fa65f57
wayungi <fwayungi@gmail.com>
2022-07-04 20:45:26 +0300

    Add deleteBook method

---
+++ modules/books.js
@@ -0,0 +1,41 @@
+let books = [
+  {
+    title: 'Java 4',
+    author: 'Agaba',
+    isbn: '65476473',
+  },
+  {
+    title: 'Python 4',
+    author: 'Bruce W',
+    isbn: '990880988',
+  },
+  {
+    title: 'JavaScript',
+    author: 'Francis',
+    isbn: '3453553565',
+  },
+];
+
+const addBook = (book) => {
+  books.push(book);
+  return book;
+};
+
+const deleteBook = (isbn) => {
+  books = books.filter((book) => book.isbn !== isbn);
+  return books;
+};
+  /*
+    * test code
+  */
+const book = {
+  title: 'test book',
+  author: 'Anniset',
+  isbn: Date.now().toString(),
+};
+console.log(books);
+addBook(book);
+console.log(books);
+const bk1 = deleteBook('65476473');
+const bk2 = deleteBook('3453553565');
+console.log(books);
