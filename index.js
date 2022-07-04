7871563278123d079ffff1cfe6f6a3a4e27ff545
wayungi <fwayungi@gmail.com>
2022-07-04 22:40:13 +0300

    Clear input fields

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ import Book from './modules/book.js';
 const form = document.getElementById('add_book_form');
 
 form.addEventListener('submit', () => {
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
 
   if (title && author) {
     const book = Book(title, author);
     addBook(book);
+
     /*
       TODO: Add interface update method here
     */
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
     return book;
   }
 });
