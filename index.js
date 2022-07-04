09e27083b8b3772d8fd30ddbdc5e8a5b672bc65d
wayungi <fwayungi@gmail.com>
2022-07-04 22:11:31 +0300

    Add implementation of add book

---
+++ index.js
@@ -0,0 +1,12 @@
+import { addBook, deleteBook, getBooks } from './modules/books.js';
+
+const form = document.getElementById('add_book_form');
+
+form.addEventListener('submit', () => {
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+
+  if (title && author) {
+    
+  }
+});
